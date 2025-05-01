71829c219a99956363c4a527ffc99a7c6df68994
Arthur Emanuel <arthurresende2004@gmail.com>
2025-05-01 09:50:14 -0300

    Correção nas rotas e remoção de rotas desnecessárias

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,6 @@ import env from './config/env';
 
 // Importando rotas
 import pacienteRoutes from './routes/paciente.routes';
-import dentistaRoutes from './routes/dentista.routes';
-import enderecoRoutes from './routes/endereco.routes';
 import topicoRoutes from './routes/topico.routes';
 import cardRoutes from './routes/card.routes';
 import imagemRoutes from './routes/imagem.routes';
@@ -23,8 +21,6 @@ setupRoutes(app);
 
 // Rotas
 app.use('/api/pacientes', pacienteRoutes);
-app.use('/api/dentistas', dentistaRoutes);
-app.use('/api/enderecos', enderecoRoutes);
 app.use('/api/topicos', topicoRoutes);
 app.use('/api/cards', cardRoutes);
 app.use('/api/imagens', imagemRoutes);
