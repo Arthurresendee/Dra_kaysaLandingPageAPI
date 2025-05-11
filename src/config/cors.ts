19165a0fa8315e352058f6f6ca5f1e9e1e4aaf6b
Arthur Emanuel <arthurresende2004@gmail.com>
2025-05-11 13:17:11 -0300

    Adicionando permissão de cors para porta 174 e 175

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 const corsConfig = cors({
   origin: process.env.NODE_ENV === 'production' 
     ? ['https://drakaysa.com.br', 'http://localhost:5173']
-    : ['http://localhost:5173', 'http://localhost:5174'],
+    : ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
