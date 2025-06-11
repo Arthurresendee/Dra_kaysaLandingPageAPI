56e279cf7b2acbd4fde074c21cfe31c82c882f2f
Arthur Emanuel <arthurresende2004@gmail.com>
2025-06-11 20:57:09 -0300

    Adicionando origem cors permitida

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,12 +2,13 @@ import cors from 'cors';
 
 const corsConfig = cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://drakaysa.com.br', 'https://adm.drakaysa.com.br', 'http://localhost:5173']
+    ? ['https://drakaysa.com.br', 'https://adm.drakaysa.com.br', 'https://api.drakaysa.com.br']
     : ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+  exposedHeaders: ['Content-Range', 'X-Content-Range'],
   credentials: true,
-  maxAge: 600
+  maxAge: 86400 // 24 horas em segundos
 });
 
 export default corsConfig; 
